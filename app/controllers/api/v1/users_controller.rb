df92dc225ff91c57d8d3f6b11411a249359ec2da
slamet <slametnhd@gmail.com>
2013-02-19 08:12:05 -0800

    Update app/controllers/api/v1/users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,3 @@
 
 
-module Api
-  module V1
-    class UsersController &amp;lt; ApplicationController
-      http_basic_authenticate_with name: &amp;quot;admin&amp;quot;, password: &amp;quot;secret&amp;quot;
-      respond_to :json
-
-      # Actions omitten
-    end
-  end
-end
 
