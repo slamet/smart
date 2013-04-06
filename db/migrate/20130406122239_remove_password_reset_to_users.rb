52fa3c867be66e21cdd405368ecb500caa90f226
slamet <slametnhd@gmail.com>
2013-04-06 07:48:04 -0700

    admin

--- db/migrate/20130406122239_remove_password_reset_to_users.rb
+++ db/migrate/20130406122239_remove_password_reset_to_users.rb
@@ -2,6 +2,7 @@ class RemovePasswordResetToUsers < ActiveRecord::Migration
   def change
   	remove_column :users, :password_reset_token, :string
 
+
     remove_column :users, :password_reset_sent_at, :datetime
   end
 
